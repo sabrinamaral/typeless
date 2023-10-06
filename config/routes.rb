102d66e5abb7f8a781e2c195c95593acc73b7d7c
sabrina amaral <sabrinamaraldev@gmail.com>
2023-10-06 14:53:05 -0300

    add reports route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :expenses
   resources :incomes
+  resources :reports, only: :index
   get "new", to: "pages#new", as: :new_pages
   post "", to: "pages#create"
 end
